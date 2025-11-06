dc7ed9c9754bdda5641f9be7132c7ec11e73e9b3
FaraNav <farruxnuranov2001@gmail.com>
2025-11-06 15:24:51 +0500

    fara

--- src/components/ContactCTA/ContactCTA.jsx
+++ src/components/ContactCTA/ContactCTA.jsx
@@ -7,7 +7,7 @@ function ContactCTA() {
       <div className="container">
         <div className={styles.box}>
           <div className={styles.wrap}>
-            <div>
+            <div className={styles.text}>
               <div className={styles.badge}>ALOQA</div>
               <h3 className={styles.title}>Xozroq ariza qoldiring va 5ta darsni bepulga oling!</h3>
               <p className={styles.subtitle}>Aloqa uchun so’rovni to’ldiring — yaqin orada siz bilan bog’lanamiz.</p>
